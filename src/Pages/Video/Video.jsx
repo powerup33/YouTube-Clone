ae25b75535031a0b4eddbf130966cd3ce34328b0
powerup33 <powerupcapital33@gmail.com>
2025-12-16 19:02:15 -0600

    half way  thru video page

--- src/Pages/Video/Video.jsx
+++ src/Pages/Video/Video.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
-// import './Video.css'
+import PlayVideo from '../../Components/PlayVideo/PlayVideo'
+import './Video.css'
 
 const Video = () => {
   return (
-    <div>
-      
+    <div className='play-container'>
+      <PlayVideo />
     </div>
   )
 }
